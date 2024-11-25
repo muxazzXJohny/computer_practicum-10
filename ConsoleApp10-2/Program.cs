3f63d81abdd64fc62eee85462f31aece4ac2b19f
muxazzXJohny <maracoco334@gmail.com>
2024-11-25 18:43:52 +0200

    task-2

---
+++ ConsoleApp10-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Input();
+        }
+        /// <summary>
+        /// Метод вывода
+        /// </summary>
+        public static void Input()
+        {
+            Console.Write($"x={Math.Round(Func(5) + Func(12) + Func(19), 2)}");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Метод функции
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static double Func(int a)
+        {
+            return (Math.Sqrt(a) + a) / 2;
+        }
+    }
+}
