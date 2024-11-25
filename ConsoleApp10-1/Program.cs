d78ab104b4e5dd3f90bb50ee755939b2a07c435a
muxazzXJohny <maracoco334@gmail.com>
2024-11-25 18:41:58 +0200

    task-1

---
+++ ConsoleApp10-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1_10
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{Math.Round(Func(5)) + Math.Round(Func(12)) + Math.Round(Func(19),2)}");
+            Console.ReadKey();
+        }
+        public static double Func(int x)
+        {
+            return (Math.Sqrt(x) + x) / 2;
+        }
+
+    }
+}
